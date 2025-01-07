c9f676ec0f632144f0ed2adbf97dab742806c1f9
datph <phungdat0902@gmail.com>
2025-01-07 10:29:50 +0700

    add feature user

---
+++ components/feature/user/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const index: React.FC = () => {
+    return (
+        <div>
+            <h1>index Component</h1>
+        </div>
+    );
+};
+
+export default index;
